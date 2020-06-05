1d685a127f432442bce4239ed0b38a38bb85955d
MAYUKH MITRA <64422986+Riju98@users.noreply.github.com>
2020-06-05 17:28:59 +0530

    Fourth Commit

--- src/main/java/com/thinkingstack/swaggyplus/Controller/Usercontroller.java
+++ src/main/java/com/thinkingstack/swaggyplus/Controller/Usercontroller.java
@@ -15,6 +15,7 @@ import com.thinkingstack.swaggyplus.Repository.Userrepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:4200")
 public class Usercontroller {
    @Autowired
    private Userrepo user;
